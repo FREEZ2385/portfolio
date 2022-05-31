9ffd9d5be225f6cfd3f555ee8a73fd0972aee621
FREEZ2385 <schlee9703@naver.com>
2022-05-31 20:10:58 +0900

    [Update] save to development

--- src/components/atoms/scrollIcon.js
+++ src/components/atoms/scrollIcon.js
@@ -4,7 +4,7 @@ import "./scss/scrollIcon.scss";
 function ScrollIcon() {
   return (
     <div className="scroll-icon">
-      <div className="content">Scroll Please</div>
+      <div className="content">Scroll Please!</div>
       <div className="scroll"></div>
     </div>
   );
