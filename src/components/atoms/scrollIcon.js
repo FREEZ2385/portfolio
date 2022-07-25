baca09aa1f791016dd11c4d777565b04ad3b5ae3
Freez <schlee9703@naver.com>
2022-07-25 21:59:25 +0900

    [Update] scroll icon invisibly if scroll was down

--- src/components/atoms/scrollIcon.js
+++ src/components/atoms/scrollIcon.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./scss/scrollIcon.scss";
 
 function ScrollIcon() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log(window.scrollX, window.scrollY);
+      console.log(window.outerHeight);
+      if (window.scrollY < window.outerHeight + 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    });
+  }, []);
   return (
-    <div className="scroll-icon">
+    <div className={isVisible ? "scroll-icon" : "scroll-icon invisible"}>
       <div className="content">Scroll Please!</div>
       <div className="scroll"></div>
     </div>
