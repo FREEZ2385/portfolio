5b5e26f9649149684ef4a184c7248f0b5edcf187
Freez <schlee9703@naver.com>
2022-07-25 19:07:43 +0900

    [Update] add contents of about me

--- src/components/templates/aboutMe.js
+++ src/components/templates/aboutMe.js
@@ -2,7 +2,8 @@
 import React from "react";
 import "aos/dist/aos.css";
 import "./scss/aboutMe.scss";
-import aboutMePicure from "../../images/aboutMe/introduce.png";
+import aboutMePicure from "../../images/aboutMe/introduce.jpeg";
+import { Grid } from "@mui/material";
 
 function AboutMe() {
   return (
@@ -13,19 +14,56 @@ function AboutMe() {
         data-aos-offset="200"
         className="about-me-content-area"
       >
-        <div className="about-me-title">About Me</div>
         <div className="about-me-picture-area">
           <div className="about-me-picture">
-            <img src={aboutMePicure} style={{ maxHeight: "40vh" }} />
+            <img
+              src={aboutMePicure}
+              style={{ maxHeight: "40vh", borderRadius: "3vmax" }}
+            />
           </div>
           <div className="about-me-simple-introduce">
-            <div className="introduce-name">Lee Sangcheol</div>
-            ３年目の韓国出身エンジニア
-            <br />
+            <div className="introduce-name">About Me</div>
+            <div className="introduce-nickname">Lee Freezを紹介します。</div>
+            <div className="introduce-subtitle">
+              <Grid container>
+                <Grid item xs={4}>
+                  名前
+                </Grid>
+                <Grid item xs={8}>
+                  : Lee Sangcheol(イーサンチョル)
+                </Grid>
+                <Grid item xs={4}>
+                  国籍
+                </Grid>
+                <Grid item xs={8}>
+                  : 韓国
+                </Grid>
+                <Grid item xs={4}>
+                  日本語能力(JLPT)
+                </Grid>
+                <Grid item xs={8}>
+                  : ネイティブ(N2)
+                </Grid>
+                <Grid item xs={4}>
+                  趣味
+                </Grid>
+                <Grid item xs={8}>
+                  : サイクリング、運動
+                </Grid>
+              </Grid>
+            </div>
           </div>
         </div>
         <div className="about-me-introduce-area">
-          <div>Lee Sangcheol</div>
+          <div>
+            こんにちは。Lee Sangcheol(イー・サンチョル)と申します。
+            <br />
+            日本に来てから3年目になってました。大学を卒業した後日本の生活を憧れて日本に行ってお仕事を始めました。
+            大学から「Web
+            UI」、「プログラミング」、「開発」に関するものを勉強して、この技術をもって日本の方と働いながら役になれるように頑張っています。
+            日常に不便なものを私の技術内で作るのが好きです。新しいアプリを作るため、いつも新しいものを挑戦するのを好きなので新しい技術を探しに行くし、興味があったら独学でやってみます。
+            いつかウェブアプリケーション以外にもAIに関しても軽く勉強してみたいです。
+          </div>
         </div>
       </div>
     </section>
